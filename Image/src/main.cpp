23938586be5e832cb9182bc91c1443131e5760c9
terrinamatsu <jacobworgandx@gmail.com>
2021-10-21 15:55:16 +0100

    RGBA working default image ctor, added failing test for Image user ctor

---
+++ Image/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout<<"Image \n";
+    
+    return EXIT_SUCCESS;
+}
---
+++ Image/tests/ImageTests.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <Image.h>
+
+TEST(RGBA, ctor)
+{
+    RGBA a;
+    ASSERT_EQ(a.r, 0);
+    ASSERT_EQ(a.g, 0);
+    ASSERT_EQ(a.b, 0);
+    ASSERT_EQ(a.a, 255);
+}
+
+TEST(RGBA, usr_ctor)
+{
+    RGBA a(255, 128, 64, 55);
+    ASSERT_EQ(a.r, 255);
+    ASSERT_EQ(a.g, 128);
+    ASSERT_EQ(a.b, 64);
+    ASSERT_EQ(a.a, 55);
+}
+
+TEST(IMAGE, ctor)
+{
+    Image img;
+    ASSERT_EQ(img.width(), 0);
+    ASSERT_EQ(img.height(), 0);
+}
+
+TEST(IMAGE, usr_ctor)
+{
+    Image img(50, 50);
+}
